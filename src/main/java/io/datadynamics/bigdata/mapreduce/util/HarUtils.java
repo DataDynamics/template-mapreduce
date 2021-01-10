f961236e3152253207ffa507a5ccb059d40617d0
김병곤 <fharenheit@gmail.com>
2021-01-10 14:40:16 +0900

    Util 추가

--- src/main/java/io/datadynamics/bigdata/mapreduce/util/HarUtils.java
+++ src/main/java/io/datadynamics/bigdata/mapreduce/util/HarUtils.java
@@ -1,7 +1,21 @@
 package io.datadynamics.bigdata.mapreduce.util;
 
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.HarFileSystem;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class HarUtils {
 
-
+    public static HarFileSystem get(FileSystem fs, String harPath) throws IOException, URISyntaxException {
+        HarFileSystem harFs = new HarFileSystem(fs);
+        String hdfsPath = fs.getConf().get("fs.default.name");
+        String harScheme = StringUtils.replace(hdfsPath, "hdfs", "har");
+        URI uri = new URI(harScheme + harPath);
+        harFs.initialize(uri, fs.getConf());
+        return harFs;
+    }
 
 }
--- src/test/java/io/datadynamics/bigdata/mapreduce/util/HarUtilsTest.java
+++ src/test/java/io/datadynamics/bigdata/mapreduce/util/HarUtilsTest.java
@@ -1,14 +1,25 @@
 package io.datadynamics.bigdata.mapreduce.util;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.HarFileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 public class HarUtilsTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, URISyntaxException {
         Configuration conf = new Configuration();
         FileSystem localFileSystem = FileSystem.getLocal(conf);
         localFileSystem.initialize(new URI("file:///"), conf);
 
-        final Path indexPath = new Path(aHarPath, "_index");
-        final Path masterIndexPath = new Path(aHarPath, "_masterindex");
+        String harPath = "D:/Projects/template-mapreduce/src/test/resources/test.har";
+
+        HarFileSystem harFs = HarUtils.get(localFileSystem, harPath);
+        System.out.println(harFs.getHarVersion());
+
+        System.out.println(harFs.exists(new Path(harPath + "/dir1")));
     }
 }
